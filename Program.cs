3d1ed70802d200d74d34a347abc22234d488f049
Jordan Hales <jordanhales@gmail.com>
2019-08-02 20:51:43 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WRLD_Programming_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileContent = File.ReadAllText(@"problem_small.txt");
+            var array = fileContent.Split((string[])null, StringSplitOptions.RemoveEmptyEntries);
+            
+            foreach (string line in array)
+            {
+                // Use a tab to indent each line of the file.
+                Console.WriteLine(line);
+            }
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# WRLD_Programming_Test
+ Test application for a software developer position at WRLD3D
