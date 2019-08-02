5da47017d2b735715e9b5f3a6c9be3650bf6e1e0
Jordan Hales <jordanhales@gmail.com>
2019-08-02 21:02:51 +0100

    Classes created
    Vector2 and Node class with variables and functions

---
+++ Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WRLD_Programming_Test
+{
+    class Node
+    {
+        public Vector2 position;
+        public Node parent;
+        public Node child;
+        
+        public float CalculateDistance(Node point)
+        {
+            return (float)Math.Sqrt(Math.Pow(position.x - point.position.x, 2) + Math.Pow(position.y - point.position.y, 2));
+        }
+    }
+}
---
+++ Vector2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WRLD_Programming_Test
+{
+    class Vector2
+    {
+        public float x;
+        public float y;
+    }
+}
